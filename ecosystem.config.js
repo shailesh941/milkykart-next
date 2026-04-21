e8dc02bfe2ede8219bb55a89f06a8d1ee1ee12db
shaileshsmartworking <30897581+shailesh941@users.noreply.github.com>
2026-04-22 00:07:47 +0530

    add new change
    add new change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,11 @@ module.exports = {
   apps: [
     {
       name: "milkykart-next",
-      script: "npm",
-      args: "start",
-      cwd: "/var/www/milkykart-next", // your project path
-      instances: 1,
-      exec_mode: "fork",
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "300M",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      cwd: "/var/www/milkykart-next",
       env: {
-        NODE_ENV: "production",
-        PORT: 3000
+        NODE_ENV: "production"
       }
     }
   ]
