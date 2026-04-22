55f63063450211f2ceaac888d594a01482e3be11
shaileshsmartworking <30897581+shailesh941@users.noreply.github.com>
2026-04-22 22:42:35 +0530

    change config
    change config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'localhost',
+        hostname: '13.60.204.10',
         pathname: '/**',
       },
     ],
   },
-  
+
+  generateBuildId: async () => {
+    return Date.now().toString();
+  },
 };
 
 export default nextConfig;
