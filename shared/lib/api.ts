66a3e31625d93e35e84ad9fb5565c92504e90e19
shaileshsmartworking <30897581+shailesh941@users.noreply.github.com>
2026-04-24 00:01:03 +0530

    add prod build

--- shared/lib/api.ts
+++ shared/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'http://35.238.68.189/api',
   headers: {
     'Content-Type': 'application/json',
   },
